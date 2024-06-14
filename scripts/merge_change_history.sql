dd9d84e4c028e9c9e5e51cc0313b6ac2c440afd7
davidsun202206 <david.sun@ca.ey.com>
2024-06-14 13:21:33 -0400

    convert to sql files

---
+++ scripts/merge_change_history.sql
@@ -0,0 +1,37 @@
+MERGE INTO SCHEMACHANGE.CHANGE_HISTORY target
+USING SCHEMACHANGE.CHANGE_HISTORY_$subdir source 
+ON target.SCRIPT = source.SCRIPT AND target.PROJECT = '$subdir' 
+WHEN MATCHED AND (target.SCRIPT_TYPE = 'A' OR (target.SCRIPT_TYPE = 'R' AND target.CHECKSUM != source.CHECKSUM))
+    THEN UPDATE SET 
+    target.CHECKSUM = source.CHECKSUM
+    ,target.EXECUTION_TIME = source.EXECUTION_TIME
+    ,target.STATUS = source.STATUS
+    ,target.INSTALLED_BY = source.INSTALLED_BY
+    ,target.INSTALLED_ON = source.INSTALLED_ON
+    ,target.PROJECT = '$subdir'            
+WHEN NOT MATCHED 
+    THEN INSERT (
+    VERSION
+    ,DESCRIPTION
+    ,SCRIPT
+    ,SCRIPT_TYPE
+    ,CHECKSUM
+    ,EXECUTION_TIME
+    ,STATUS
+    ,INSTALLED_BY
+    ,INSTALLED_ON
+    ,PROJECT
+    ) 
+    VALUES (
+    source.VERSION
+    ,source.DESCRIPTION
+    ,source.SCRIPT
+    ,source.SCRIPT_TYPE
+    ,source.CHECKSUM
+    ,source.EXECUTION_TIME
+    ,source.STATUS
+    ,source.INSTALLED_BY
+    ,source.INSTALLED_ON
+    ,'$subdir'
+    );
+    DROP TABLE SCHEMACHANGE.CHANGE_HISTORY_$subdir;
